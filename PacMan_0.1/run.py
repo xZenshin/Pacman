26e998d6813d09bb40682b5845342375c0614503
alsk <alsk@itu.dk>
2024-02-07 16:01:27 +0100

    add up until lvl 2

--- PacMan_0.1/run.py
+++ PacMan_0.1/run.py
@@ -1,12 +1,15 @@
 import pygame
 from pygame.locals import *
 from constants import *
+from pacman import Pacman
+from nodes import NodeGroup
 
 class GameController(object):
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode(SCREENSIZE, 0, 32)
         self.background = None
+        self.clock = pygame.time.Clock()
 
     def setBackground(self):
         self.background = pygame.surface.Surface(SCREENSIZE).convert()
@@ -14,8 +17,12 @@ class GameController(object):
 
     def startGame(self):
         self.setBackground()
+        self.nodes = NodeGroup("maze1.txt")
+        self.pacman = Pacman(self.nodes.getStartTempNode())
 
     def update(self):
+        dt = self.clock.tick(30) / 1000.0
+        self.pacman.update(dt)
         self.checkEvents()
         self.render()
 
@@ -25,6 +32,9 @@ class GameController(object):
                 exit()
 
     def render(self):
+        self.screen.blit(self.background, (0, 0))
+        self.nodes.render(self.screen)
+        self.pacman.render(self.screen)
         pygame.display.update()
 
     
